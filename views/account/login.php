7cd62221646ea48f7d0c1630629def964aded2ec
Makis Tracend <makis.tracend@gmail.com>
2015-04-25 21:42:11 -0700

    + views

---
+++ views/account/login.php
@@ -0,0 +1,8 @@
+<?php
+//
+?>
+<form action="<? url("account/login"); ?>" method="POST">
+	<input type="text" name="email" placeholder="Email address">
+	<input type="password" name="password" placeholder="Password">
+	<input type="submit">
+</form>
---
+++ views/account/register.php
@@ -0,0 +1,10 @@
+<?php
+//
+?>
+<form action="<? url("account/register"); ?>" method="POST">
+	<input type="text" name="fullname" placeholder="Full name">
+	<input type="text" name="email" placeholder="Email address">
+	<input type="password" name="password" placeholder="Password">
+	<input type="password" name="password_repeat" placeholder="Password repeat">
+	<input type="submit">
+</form>
